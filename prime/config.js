a7ab1ebb24f2ef385f69f72e68f210ef95bac8b3
𝙍𝙄𝙎𝙃𝙄 𝙁𝙊𝙍𝙂𝙀 <sutapaditti9@gmail.com>
2026-08-04 22:19:28 +0530

    Add files via upload

---
+++ prime/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  // आपका JSON Endpoint
+  JSON_URL: "https://sonujson-devloper.vercel.app/Data/prime.json",
+  
+  // लोगो URLs
+  TITU_LOGO: "https://assetstitu.vercel.app/TituLogo.svg",
+  EV_LOGO: "https://raw.githubusercontent.com/saptarshiorg/saptarshiorg.github.io/refs/heads/main/magenta.png",
+  FANCODE_LOGO: "https://www.fancode.com/skillup-uploads/fc-web-logo/fc_logo_white_bg.svg",
+  FALLBACK_IMG: "https://www.fancode.com/skillup-uploads/cms-media/Cricket_Fallback_Old_match-card.jpg",
+  
+  // JW Player Key
+  JW_KEY: "XSuP4qMl+9tK17QNb+4+th2Pm9AWgMO/cYH8CI0HGGr7bdjo",
+  
+  // एन्क्रिप्शन टोकन सुरक्षा
+  SECRET_SALT: "TITU_EV_SECURE_2026"
+};
